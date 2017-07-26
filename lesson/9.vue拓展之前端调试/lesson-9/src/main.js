e6b5fa6741bf571611c3d41157f7a5919f3f087f
liuxuan <petersmu@163.com>
2017-07-26 16:25:03 +0800

    lesson9

---
+++ lesson/9.vue拓展之前端调试/lesson-9/src/main.js
@@ -0,0 +1,104 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import FastClick from 'fastclick'
+import App from './App'
+import router from './router'
+import Axios from 'axios'
+import './mock'
+import Vuex from 'vuex'
+import { ToastPlugin, LoadingPlugin } from 'vux'
+import * as util from './util.js'
+// import {foo} from './util.js'
+console.log(util)
+// foo()
+// util.fun()
+
+Vue.use(Vuex) // 在vue实例中加入vuex插件
+Vue.use(ToastPlugin)
+Vue.use(LoadingPlugin)
+
+FastClick.attach(document.body)
+
+Vue.config.productionTip = false
+
+// Vue.prototype.$axios = Axios
+
+// 带配置的Axios
+Vue.prototype.$axios = Axios.create({
+  baseURL: 'http://localhost:8000/'
+})
+
+const store = new Vuex.Store({
+  state: {
+    todoList: [{
+      'todo': '待办1',
+      'done': false
+    },
+    {
+      'todo': '待办2',
+      'done': false
+    },
+    {
+      'todo': '待办3',
+      'done': false
+    },
+    {
+      'todo': '待办4',
+      'done': false
+    },
+    {
+      'todo': '待办5',
+      'done': false
+    }]
+  },
+  getters: {
+    undos (state) {
+      return state.todoList.filter(todo => !todo.done)
+    }
+  },
+  mutations: {
+    TODO_ADD (state, payload) {
+      state.todoList.push(payload.todo)
+    },
+    TODO_DONE (state, payload) {
+      state.todoList[payload.index].done = true
+    },
+    TODO_DEL (state, payload) {
+      state.todoList.splice(payload.index, 1)
+    }
+  },
+  actions: {
+    todoAdd ({ commit }, todo) {
+      commit({
+        type: 'TODO_ADD',
+        todo: todo,
+        done: todo.done
+      })
+    },
+    todoControl ({ commit }, payload) {
+      switch (payload.cmd) {
+        case 'done':
+          commit({
+            type: 'TODO_DONE',
+            index: payload.index
+          })
+          break
+        case 'delete':
+          commit({
+            type: 'TODO_DEL',
+            index: payload.index
+          })
+          break
+        default:
+          break
+      }
+    }
+  }
+})
+/* eslint-disable no-new */
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app-box')
---
+++ lesson/9.vue拓展之前端调试/lesson-9/src/pages/Todo2.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="todo">
+    <swipeout>
+      <swipeout-item v-for="(item,index) in todoList"
+                     :key="index"
+                     :disabled="item.done"
+                     ref="swipeoutItem"
+                     :right-menu-width="140"
+                     :sensitivity="15">
+        <div slot="right-menu">
+          <swipeout-button @click.native="todoControl({index: index, cmd: 'delete'})"
+                           type="warn"
+                           :width="70">删除</swipeout-button>
+          <swipeout-button @click.native="onEditClick(index)"
+                           type="default"
+                           :width="70">编辑</swipeout-button>
+        </div>
+  
+        <div slot="left-menu">
+          <swipeout-button @click.native="todoControl({index: index, cmd: 'done'})"
+                           type="primary">完成</swipeout-button>
+        </div>
+  
+        <div slot="content"
+             class="demo-content vux-1px-b"
+             :class="item.done ? 'completed' : ''">
+          {{item.todo}}
+        </div>
+      </swipeout-item>
+    </swipeout>
+    <group :title="'剩余'+todoUndo.length">
+      <x-input title="新待办"
+               v-model="newTodo"></x-input>
+    </group>
+    <box gap="10px">
+      <x-button type="primary"
+                @click.native="addTodo"
+                class="my-btn">添加待办</x-button>
+    </box>
+    <div v-transfer-dom>
+      <popup v-model="popShow"
+             position="bottom"
+             max-height="50%">
+        <group>
+          <x-input title="修改"
+                   v-model="editTodo"></x-input>
+        </group>
+        <div style="padding: 15px;">
+          <x-button @click.native="editTodos"
+                    plain
+                    type="primary">确认修改</x-button>
+        </div>
+      </popup>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  Group,
+  Cell,
+  XInput,
+  XButton,
+  Swipeout,
+  SwipeoutItem,
+  SwipeoutButton,
+  Box,
+  TransferDom,
+  Popup
+} from 'vux'
+import { mapState, mapGetters, mapActions } from 'vuex'
+
+export default {
+  directives: {
+    TransferDom
+  },
+  components: {
+    Group,
+    Cell,
+    XInput,
+    XButton,
+    Swipeout,
+    SwipeoutItem,
+    SwipeoutButton,
+    Box,
+    Popup
+  },
+  data () {
+    return {
+      newTodo: '',
+      editTodo: '',
+      popShow: false,
+      editNum: 0
+    }
+  },
+  computed: {
+    ...mapState(['todoList']),
+    ...mapGetters({
+      todoUndo: 'undos'
+    })
+  },
+  methods: {
+    ...mapActions(['todoDone', 'todoAdd', 'todoControl']),
+    addTodo () {
+      if (this.newTodo) {
+        const t = {
+          todo: this.newTodo,
+          done: false
+        }
+        this.todoAdd(t)
+        this.newTodo = ''
+      }
+    },
+    onEditClick (i) {
+      this.editNum = i
+      this.editTodo = this.todoList[i].todo
+      this.popShow = true
+    },
+    editTodos () {
+      if (this.editTodo) {
+        this.todoList[this.editNum].todo = this.editTodo
+        this.popShow = false
+        this.editTodo = ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.demo-content {
+  line-height: 40px;
+  padding: 0 10px;
+}
+
+.completed {
+  text-decoration: line-through;
+  color: #666;
+}
+
+.my-btn {
+  background-color: #000;
+  height: 90px;
+}
+</style>
+
